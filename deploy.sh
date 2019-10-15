b7ddb749679b0208879d37880d0e6a6f313b166f
qeknxu <qeknxu@gmail.com>
2019-10-15 22:17:33 +0800

    部署测试

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,27 @@
-//deploy.sh
 #!/usr/bin/env sh
-# 當發生錯誤時終止腳本運行
-set -e
-# 打包
-npm run build
-# 移動至到打包後的dist目錄 
-cd dist
-git init //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
-git add -A
-git commit -m 'deploy'
 
-# 部署到     https://github.com/qeknxu/henry-projects.git 分支為 gh-pages
-git push -f https://github.com/qeknxu/henry-projects.git master:gh-pages
-//將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
+# 当发生错误时中止脚本
+set -e
+
+# 构建
+npm run build
+
+# cd 到构建输出的目录下 
+# cd dist
+
+# 部署到自定义域域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m '部署测试'
+
+# 部署到 https://<USERNAME>.github.io
+# git push -f git@github.com:xjinky/xjinky.github.io.git master
+
+# 部署到 https://<USERNAME>.github.io/<REPO>
+# git push -f https://github.com/qeknxu/henry-projects.git master:gh-pages  # 提交到gh-master分支
+
+git push -f https://github.com/qeknxu/henry-projects.git master    # 提交到master分支
 
 cd -
